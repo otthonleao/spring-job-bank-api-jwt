e15e6b2b3c9cb48314681dc24c8efe4dd93c3b2c
Otthon Leão <otthonleao@hotmail.com>
2024-06-20 10:48:17 -0400

    Initial commit

---
+++ src/main/java/dev/otthon/jobbank/JobBankApplication.java
@@ -0,0 +1,13 @@
+package dev.otthon.jobbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobBankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/otthon/jobbank/JobBankApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.otthon.jobbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
