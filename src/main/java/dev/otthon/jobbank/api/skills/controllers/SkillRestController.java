1d435f3e946032ff718d4039a03666f0dcd90d6e
Otthon Leão <otthonleao@hotmail.com>
2024-06-21 09:45:37 -0400

    chore: add CREATE, UPDATE and DELETE for SkillRestController

--- src/main/java/dev/otthon/jobbank/api/skills/controllers/SkillRestController.java
+++ src/main/java/dev/otthon/jobbank/api/skills/controllers/SkillRestController.java
@@ -1,14 +1,15 @@
 package dev.otthon.jobbank.api.skills.controllers;
 
+import dev.otthon.jobbank.api.skills.dtos.SkillRequestDTO;
 import dev.otthon.jobbank.api.skills.dtos.SkillResponseDTO;
 import dev.otthon.jobbank.api.skills.mappers.SkillMapper;
 import dev.otthon.jobbank.core.exceptions.SkillNotFoundException;
 import dev.otthon.jobbank.core.repositories.SkillRepository;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.BeanUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -33,6 +34,31 @@ public class SkillRestController {
         return skillRepository.findById(id)
                 .map(skillMapper::toSkillResponseDTO)
                 .orElseThrow(SkillNotFoundException::new);
+    }
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public SkillResponseDTO create(@RequestBody @Valid SkillRequestDTO skillRequestDTO) {
+        var skill = skillMapper.toSkill(skillRequestDTO);
+        skill = skillRepository.save(skill);
+        return skillMapper.toSkillResponseDTO(skill);
+    }
+
+    @PutMapping("/{id}")
+    public SkillResponseDTO update(@PathVariable Long id, @Valid @RequestBody SkillRequestDTO skillRequestDTO) {
+        var skill = skillRepository.findById(id)
+                .orElseThrow(SkillNotFoundException::new);
+        BeanUtils.copyProperties(skillRequestDTO, skill, "id");
+        skill = skillRepository.save(skill);
+        return skillMapper.toSkillResponseDTO(skill);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id) {
+        var skill = skillRepository.findById(id)
+                .orElseThrow(SkillNotFoundException::new);
+        skillRepository.delete(skill);
     }
 
 }
