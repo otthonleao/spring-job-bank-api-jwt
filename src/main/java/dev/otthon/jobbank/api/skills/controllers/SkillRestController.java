22116e60b273b503487022a5a7c4cf20862df3c4
Otthon Leão <otthonleao@hotmail.com>
2024-06-25 13:49:56 -0400

    chore: add Spring Hateoas and including in SkillRestController GET and PUT

--- src/main/java/dev/otthon/jobbank/api/skills/controllers/SkillRestController.java
+++ src/main/java/dev/otthon/jobbank/api/skills/controllers/SkillRestController.java
@@ -8,7 +8,9 @@ import dev.otthon.jobbank.core.repositories.SkillRepository;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.BeanUtils;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,17 +25,57 @@ public class SkillRestController {
 
     @GetMapping
     public List<SkillResponseDTO> findAll() {
-        return skillRepository.findAll()
+        var skills =  skillRepository.findAll()
                 .stream()
                 .map(skillMapper::toSkillResponseDTO)
                 .toList();
+        // Montando o HATEOAS
+        skills.forEach(skill -> {
+            var id = skill.getId();
+            var selfLink = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder
+                    .methodOn(SkillRestController.class)
+                    .findById(id)
+            ).withSelfRel().withType("GET");
+
+            var updateLink = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder
+                    .methodOn(SkillRestController.class)
+                    .update(id, null)
+            ).withRel("update").withType("PUT");
+
+            var deleteLink = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder
+                    .methodOn(SkillRestController.class)
+                    .update(id, null)
+            ).withRel("update").withType("DELETE");
+
+            skill.add(selfLink, updateLink, deleteLink);
+        });
+        return skills;
     }
 
     @GetMapping("/{id}")
     public SkillResponseDTO findById(@PathVariable Long id) {
-        return skillRepository.findById(id)
+        var skill = skillRepository.findById(id)
                 .map(skillMapper::toSkillResponseDTO)
                 .orElseThrow(SkillNotFoundException::new);
+        // Montando o HATEOAS
+        var selfLink = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder
+                .methodOn(SkillRestController.class)
+                .findById(id)
+        ).withSelfRel().withType("GET");
+
+        var updateLink = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder
+                 .methodOn(SkillRestController.class)
+                 .update(id, null)
+        ).withRel("update").withType("PUT");
+
+        var deleteLink = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder
+                 .methodOn(SkillRestController.class)
+                 .update(id, null)
+        ).withRel("update").withType("DELETE");
+
+        skill.add(selfLink, updateLink, deleteLink);
+
+        return skill;
     }
 
     @PostMapping
@@ -54,11 +96,11 @@ public class SkillRestController {
     }
 
     @DeleteMapping("/{id}")
-    @ResponseStatus(code = HttpStatus.NO_CONTENT)
-    public void delete(@PathVariable Long id) {
+    public ResponseEntity<?> delete(@PathVariable Long id) {
         var skill = skillRepository.findById(id)
                 .orElseThrow(SkillNotFoundException::new);
         skillRepository.delete(skill);
+        return ResponseEntity.noContent().build();
     }
 
 }
--- src/main/java/dev/otthon/jobbank/api/skills/dtos/SkillResponseDTO.java
+++ src/main/java/dev/otthon/jobbank/api/skills/dtos/SkillResponseDTO.java
@@ -1,13 +1,14 @@
 package dev.otthon.jobbank.api.skills.dtos;
 
 import lombok.*;
+import org.springframework.hateoas.RepresentationModel;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@EqualsAndHashCode(callSuper = false)
-public class SkillResponseDTO {
+@EqualsAndHashCode(callSuper = false) // Para eliminar o warning do @Data causado por add o extends
+public class SkillResponseDTO extends RepresentationModel<SkillResponseDTO> {
 
     private Long id;
 
