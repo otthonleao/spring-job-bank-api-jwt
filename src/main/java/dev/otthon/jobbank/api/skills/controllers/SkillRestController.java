33e71f380e7ef6ab846c9a538c95ccb15f68703b
Otthon Leão <otthonleao@hotmail.com>
2024-06-26 10:15:57 -0400

    feat: add assemblers skills for HETEOAS default

--- src/main/java/dev/otthon/jobbank/api/skills/controllers/SkillRestController.java
+++ src/main/java/dev/otthon/jobbank/api/skills/controllers/SkillRestController.java
@@ -1,5 +1,6 @@
 package dev.otthon.jobbank.api.skills.controllers;
 
+import dev.otthon.jobbank.api.skills.assemblers.SkillAssembler;
 import dev.otthon.jobbank.api.skills.dtos.SkillRequestDTO;
 import dev.otthon.jobbank.api.skills.dtos.SkillResponseDTO;
 import dev.otthon.jobbank.api.skills.mappers.SkillMapper;
@@ -8,12 +9,11 @@ import dev.otthon.jobbank.core.repositories.SkillRepository;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.BeanUtils;
-import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,77 +22,45 @@ public class SkillRestController {
 
     private final SkillMapper skillMapper;
     private final SkillRepository skillRepository;
+    private final SkillAssembler skillAssembler;
 
     @GetMapping
-    public List<SkillResponseDTO> findAll() {
+    public CollectionModel<EntityModel<SkillResponseDTO>> findAll() {
         var skills =  skillRepository.findAll()
                 .stream()
                 .map(skillMapper::toSkillResponseDTO)
                 .toList();
         // Montando o HATEOAS
-        skills.forEach(skill -> {
-            var id = skill.getId();
-            var selfLink = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder
-                    .methodOn(SkillRestController.class)
-                    .findById(id)
-            ).withSelfRel().withType("GET");
-
-            var updateLink = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder
-                    .methodOn(SkillRestController.class)
-                    .update(id, null)
-            ).withRel("update").withType("PUT");
-
-            var deleteLink = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder
-                    .methodOn(SkillRestController.class)
-                    .update(id, null)
-            ).withRel("update").withType("DELETE");
-
-            skill.add(selfLink, updateLink, deleteLink);
-        });
-        return skills;
+        return skillAssembler.toCollectionModel(skills);
     }
 
     @GetMapping("/{id}")
-    public SkillResponseDTO findById(@PathVariable Long id) {
+    public EntityModel<SkillResponseDTO> findById(@PathVariable Long id) {
         var skill = skillRepository.findById(id)
                 .map(skillMapper::toSkillResponseDTO)
                 .orElseThrow(SkillNotFoundException::new);
         // Montando o HATEOAS
-        var selfLink = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder
-                .methodOn(SkillRestController.class)
-                .findById(id)
-        ).withSelfRel().withType("GET");
-
-        var updateLink = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder
-                 .methodOn(SkillRestController.class)
-                 .update(id, null)
-        ).withRel("update").withType("PUT");
-
-        var deleteLink = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder
-                 .methodOn(SkillRestController.class)
-                 .update(id, null)
-        ).withRel("update").withType("DELETE");
-
-        skill.add(selfLink, updateLink, deleteLink);
-
-        return skill;
+        return skillAssembler.toModel(skill);
     }
 
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
-    public SkillResponseDTO create(@RequestBody @Valid SkillRequestDTO skillRequestDTO) {
+    public EntityModel<SkillResponseDTO> create(@RequestBody @Valid SkillRequestDTO skillRequestDTO) {
         var skill = skillMapper.toSkill(skillRequestDTO);
         skill = skillRepository.save(skill);
-        return skillMapper.toSkillResponseDTO(skill);
+        var skillResponse = skillMapper.toSkillResponseDTO(skill);
+
+        return skillAssembler.toModel(skillResponse);
     }
 
     @PutMapping("/{id}")
-    public SkillResponseDTO update(@PathVariable Long id, @Valid @RequestBody SkillRequestDTO skillRequestDTO) {
+    public EntityModel<SkillResponseDTO> update(@PathVariable Long id, @Valid @RequestBody SkillRequestDTO skillRequestDTO) {
         var skill = skillRepository.findById(id)
                 .orElseThrow(SkillNotFoundException::new);
         BeanUtils.copyProperties(skillRequestDTO, skill, "id");
         skill = skillRepository.save(skill);
-        return skillMapper.toSkillResponseDTO(skill);
+        var skillResponse = skillMapper.toSkillResponseDTO(skill);
+        return skillAssembler.toModel(skillResponse);
     }
 
     @DeleteMapping("/{id}")
